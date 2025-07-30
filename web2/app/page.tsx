431f169e33d22692574ebd449c0800c767d51b71
ALAMAK <157081968+aangAJA@users.noreply.github.com>
2025-07-30 10:39:43 +0700

    Add files via upload

---
+++ web2/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "./componen/navbar"
+import Header from "./componen/header"
+
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+   <Header/>
+  </>
+  )
+}
